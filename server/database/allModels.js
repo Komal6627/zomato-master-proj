3982e8585f437e68b5b8684501a9a216ccd8c05b
Komal6627 <komalkilledar1996@gmail.com>
2021-07-30 11:44:15 +0530

    add static and mrthod , refactor signip route

---
+++ server/database/allModels.js
@@ -0,0 +1,9 @@
+import { FoodModel } from "./food";
+import { ImageModel } from "./image";
+import { MenuModel } from "./menu";
+import { OrderModel } from "./order";
+import { RestaurantModels } from "./restaurant";
+import { ReviewModel } from "./reviews";
+import { UserModel} from "./user";
+
+export {FoodModel, ImageModel, MenuModel, OrderModel, RestaurantModels, ReviewModel, UserModel}
--- server/index.js
+++ server/index.js
@@ -1,19 +1,36 @@
+//Importing Env variables
+require("dotenv").config();
+
+//Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+//microservice routes
+import Auth from "./API/Auth";
+
+//Database connection
+import ConnectDB from "./database/connection";
+
 const zomato = express();
 
 //application middleware
-zomato.use(express.json);
+zomato.use(express.json());
 zomato.use(express.urlencoded({extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
 
-zomato.get("/", (req, res) => res.json({message: "Setup Success"}));
+//Application routes
+zomato.use("/auth", Auth);
 
-zomato.listen(4000,() => console.log("Server is Running 🚀"));
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
-
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed.. ")
+    )
+);
 
 
