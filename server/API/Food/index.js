0d7deed4436f8226d7fd003d02fc9467c033b52e
Komal6627 <komalkilledar1996@gmail.com>
2021-08-18 22:44:33 +0530

    add food validation

--- server/API/Food/index.js
+++ server/API/Food/index.js
@@ -6,6 +6,9 @@ import passport from "passport";
 //Database model
 import { FoodModel } from "../../database/allModels";
 
+//Validation
+import { ValidateRestaurantId, ValidateCategory} from "../../Validation/food";
+
 const Router = express.Router();
 
 /*
@@ -18,6 +21,8 @@ Method    GET
 
 Router.get("/r/:_id", async (req,res) =>{
     try {
+        await ValidateRestaurantId(req.params);
+
         const {_id} =req.params;
         const foods = await FoodModel.find({ restaurant: _id });
         return res.json({ foods });
@@ -36,6 +41,7 @@ Method    GET
 
 Router.get("/r/:category ", async (req,res) =>{
     try {
+        await ValidateCategory(req.params);
         const { category } =req.params;
         const foods = await FoodModel.find({ 
             category: { $regex: category, $option: "i"},
---
+++ server/Validation/food.js
@@ -0,0 +1,19 @@
+import joi from "joi";
+
+//restaurent
+export const ValidateRestaurantId = (resId) => {
+    const Schema = joi.object({
+        _id: joi.string().required(),
+    });
+
+    return Schema.validateAsync(resId);
+};
+
+//category
+export const ValidateCategory = (category) =>{
+    const Schema = joi.object({
+        category: joi.string().required(),
+    });
+
+    return Schema.validateAsync(category);
+}
